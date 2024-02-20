afbbb4e27f9effafdc2837f95303399097bc0e37
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-02-20 15:01:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+function submitForm() {
+            const firstName = document.getElementById("firstName").value;
+            const lastName = document.getElementById("lastName").value;
+            const phoneNumber = document.getElementById("phoneNumber").value;
+            const email = document.getElementById("email").value;
+
+            const alertMessage = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${email}`;
+            alert(alertMessage);
+
+            // // Returning false prevents form submission
+            // return false;
+        }
